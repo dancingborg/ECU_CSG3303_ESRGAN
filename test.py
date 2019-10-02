5e8ae9bb6c38e479d6f931dd8a77dcc560c20e40
dancingborg <dancingborg@users.noreply.github.com>
2019-10-02 12:14:23 +0800

    constants to upper case for pep8 conformance

--- test.py
+++ test.py
@@ -6,33 +6,33 @@ import numpy as np
 import torch
 import RRDBNet_arch as arch
 
-model_path = 'models/RRDB_ESRGAN_x4.pth'
-device = torch.device('cuda')
-# device = torch.device('cpu')
+MODEL_PATH = 'models/RRDB_ESRGAN_x4.pth'
+DEVICE = torch.device('cuda')
+# DEVICE = torch.DEVICE('cpu')
 
-test_img_folder = 'input/*'
+TEST_IMG_FOLDER = 'input/*'
 
-model = arch.RRDBNet(3, 3, 64, 23, gc=32)
-model.load_state_dict(torch.load(model_path), strict=True)
-model.eval()
-model = model.to(device)
+MODEL = arch.RRDBNet(3, 3, 64, 23, gc=32)
+MODEL.load_state_dict(torch.load(MODEL_PATH), strict=True)
+MODEL.eval()
+MODEL = MODEL.to(DEVICE)
 
-print('Model path {:s}. \nTesting...'.format(model_path))
+print('Model path {:s}. \nTesting...'.format(MODEL_PATH))
 
-idx = 0
-for path in glob.glob(test_img_folder):
-    idx += 1
+IDX = 0
+for path in glob.glob(TEST_IMG_FOLDER):
+    IDX += 1
     base = osp.splitext(osp.basename(path))[0]
-    print(idx, base)
+    print(IDX, base)
     # read images
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
-    img_LR = img_LR.to(device)
+    img_LR = img_LR.to(DEVICE)
 
     with torch.no_grad():
-        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = MODEL(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite('output/{:s}_rlt.png'.format(base), output)
