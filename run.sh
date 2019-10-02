fd3ecbe6f1e812d6fb9c08a3cb97486c5442f3e8
dancingborg <dancingborg@users.noreply.github.com>
2019-10-02 12:08:58 +0800

    working prototype

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+# clear local input and output  directories in preparation for import
+rm -rv input/*
+rm -rv output/*
+# copy basicSR resultant model if present
+cp -v ../basicSR/experiments/TRAINED/models/latest_G.pth ./models/
+# copy input files for testing
+cp -vr /volume1/transfer/Optometer/ESRGAN-Input/* ./input
+# run the test program
 python3 test.py
+# copy output files
+cp -vr ./output/* /volume1/transfer/Optometer/ESRGAN-Output
