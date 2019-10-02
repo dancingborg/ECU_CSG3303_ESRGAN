67b51b4b6a71b9cc0c7b4cfeb83a0427fb251b1e
dancingborg <dancingborg@users.noreply.github.com>
2019-10-02 12:22:33 +0800

    removed too-long lines, file now pep8 compliant

--- test.py
+++ test.py
@@ -6,18 +6,18 @@ import numpy as np
 import torch
 import RRDBNet_arch as arch
 
-MODEL_PATH = 'models/RRDB_ESRGAN_x4.pth'
-DEVICE = torch.device('cuda')
+MODEL_PATH = "models/RRDB_ESRGAN_x4.pth"
+DEVICE = torch.device("cuda")
 # DEVICE = torch.DEVICE('cpu')
 
-TEST_IMG_FOLDER = 'input/*'
+TEST_IMG_FOLDER = "input/*"
 
 MODEL = arch.RRDBNet(3, 3, 64, 23, gc=32)
 MODEL.load_state_dict(torch.load(MODEL_PATH), strict=True)
 MODEL.eval()
 MODEL = MODEL.to(DEVICE)
 
-print('Model path {:s}. \nTesting...'.format(MODEL_PATH))
+print("Model path {:s}. \nTesting...".format(MODEL_PATH))
 
 IDX = 0
 for path in glob.glob(TEST_IMG_FOLDER):
@@ -27,12 +27,14 @@ for path in glob.glob(TEST_IMG_FOLDER):
     # read images
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
-    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img = torch.from_numpy(
+            np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
     img_LR = img.unsqueeze(0)
     img_LR = img_LR.to(DEVICE)
 
     with torch.no_grad():
-        output = MODEL(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = MODEL(
+                img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite('output/{:s}_rlt.png'.format(base), output)
+    cv2.imwrite("output/{:s}_rlt.png".format(base), output)
